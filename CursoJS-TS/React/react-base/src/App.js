3c720c7382ac869fed5e9b34d7321289835b28f7
EuAndersonDev <andersonaugustorei1227@gmail.com>
2026-02-11 21:36:49 -0300

    feat, config redux saga

--- CursoJS-TS/React/react-base/src/App.js
+++ CursoJS-TS/React/react-base/src/App.js
@@ -2,7 +2,9 @@ import Routes from "./routes";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
-import store from "./store";
+
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store";
 import Header from "./components/Header";
 import {Router} from "react-router-dom";
 import history from "./services/history";
@@ -10,12 +12,14 @@ import history from "./services/history";
 function App() {
     return (
         <Provider store={store}>
-            <Router history={history}>
-                <Header />
-                <Routes />
-                <GlobalStyles />
-                <ToastContainer autoClose={3000} className="toast-container"/>
-            </Router>
+            <PersistGate persistor={persistor}>
+                <Router history={history}>
+                    <Header />
+                    <Routes />
+                    <GlobalStyles />
+                    <ToastContainer autoClose={3000} className="toast-container"/>
+                </Router>
+            </PersistGate>
         </Provider>
     );
 }
--- CursoJS-TS/React/react-base/src/store/modules/example/reducer.js
+++ CursoJS-TS/React/react-base/src/store/modules/example/reducer.js
@@ -1,15 +1,28 @@
+import * as types from "../types";
+
 const initialState = {
     botaoClicado: false,
 };
 
 function exampleReducer(state = initialState, action) {
     switch (action.type) {
-        case "BOTAO_CLICADO":
+        case types.BOTAO_CLICADO_SUCCESS: {
+            console.log("Botão clicado com sucesso");
             const newState = { ...state };
             newState.botaoClicado = !newState.botaoClicado;
             return newState;
-        default:
+        }
+        case types.BOTAO_CLICADO_FAILURE: {
+            console.log("Erro ao processar ação de clique no botão");
             return state;
+        }
+        case types.BOTAO_CLICADO_REQUEST: {
+            console.log("Ação de clique no botão disparada");
+            return state;
+        }
+        default: {
+            return state;
+        }
     }
 }
 
